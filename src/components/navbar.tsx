038e28ece01c6b84fc2cde7b053473883e11e972
Frankie <67761168+frankjlin16@users.noreply.github.com>
2022-12-04 16:53:19 -0700

    Update navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,28 +1,36 @@
 import Link from "next/link";
+import Image from "next/image";
 
 //** Navbar component */
 export default function Navbar() {
-    return (
-        <nav className="flex justify-between items-center h-16 text-lg bg-asu-maroon text-white relative shadow-sm font-mono" role="navigation">
-            <Link href="/">
-                <a className="pl-8">Home</a>
-            </Link>
-            <div className="px-4 cursor-pointer md:hidden">
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                </svg>
-            </div>
-            <div className="pr-8 md:block hidden">
-                <Link href="/help">
-                    <a className="p-4">Help</a>
-                </Link>
-                <Link href="/about">
-                    <a className="p-4">About</a>
-                </Link>
-                <Link href="/contact">
-                    <a className="p-4">Contact</a>
-                </Link>
-            </div>
-        </nav>
-    )
+  return (
+    <nav
+      className="font-mono relative flex h-16 items-center justify-start bg-asu-maroon text-lg text-white shadow-sm"
+      role="navigation"
+    >
+      <div className="ml-5">
+        <Image
+          src="/logo-light.png"
+          alt="CodeDevils Logo"
+          width="100"
+          height="41"
+        />
+      </div>
+
+      <div className="ml-4">
+        <Link href="/">
+          <a className="p-4">Home</a>
+        </Link>
+        <Link href="/about">
+          <a className="p-4">About</a>
+        </Link>
+        <Link href="/contact">
+          <a className="p-4">Contact</a>
+        </Link>
+        <Link href="/help">
+          <a className="p-4">Help</a>
+        </Link>
+      </div>
+    </nav>
+  );
 }
