d4d624514000c507937889bc2998fc5458cd138a
Frankie <67761168+frankjlin16@users.noreply.github.com>
2022-12-04 16:59:53 -0700

    Update navbar
    Logo is clickable

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,14 +8,16 @@ export default function Navbar() {
       className="font-mono relative flex h-16 items-center justify-start bg-asu-maroon text-lg text-white shadow-sm"
       role="navigation"
     >
-      <div className="ml-5">
-        <Image
-          src="/logo-light.png"
-          alt="CodeDevils Logo"
-          width="100"
-          height="41"
-        />
-      </div>
+      <Link href="/">
+        <a className="ml-5">
+          <Image
+            src="/logo-light.png"
+            alt="CodeDevils Logo"
+            width="100"
+            height="41"
+          />
+        </a>
+      </Link>
 
       <div className="ml-4">
         <Link href="/">
