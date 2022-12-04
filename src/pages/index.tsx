f525033978f34e10c3d0565aae887b3f2cc1fc5f
Frankie <67761168+frankjlin16@users.noreply.github.com>
2022-12-04 16:27:50 -0700

    Update homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,12 @@ export default function Home() {
   return (
     <>
         <Navbar />
+        <main className="flex w-full flex-1 flex-col items-center justify-center px-20 mt-5">
+            <h1 className="text-xl font-bold">Home</h1>
+            <p className="text-2xl mt-3">
+                Welcome to CodeDevils! We are a virtual club that aims to bring ASU students together.
+            </p>
+        </main>
     </>
   );
 }
