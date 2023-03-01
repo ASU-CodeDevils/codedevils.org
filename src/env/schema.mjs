30cc74e17e8fdca38fe711aeba9577597290b349
Frankie <67761168+frankjlin16@users.noreply.github.com>
2023-02-28 17:21:33 -0700

    Remove discord login provider and used server schema

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -6,12 +6,10 @@ import { z } from "zod";
  * This way you can ensure the app isn't built with invalid env vars.
  */
 export const serverSchema = z.object({
-  DATABASE_URL: z.string().url(),
+  // DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
-  NEXTAUTH_SECRET: z.string(),
-  NEXTAUTH_URL: z.string().url(),
-  DISCORD_CLIENT_ID: z.string(),
-  DISCORD_CLIENT_SECRET: z.string(),
+  // NEXTAUTH_SECRET: z.string(),
+  // NEXTAUTH_URL: z.string().url(),
 });
 
 /**
