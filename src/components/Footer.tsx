c8816a96fd5d7ffd2d570f15dab8204556f81793
David Nevarez <david.nevarez03@hotmail.com>
2023-07-07 14:29:42 -0700

    refactored code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,137 +2,8 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-import { useInView } from "react-intersection-observer";
-
-// styles
-import desktopStyles from "./../../styles/components/page_layout/footers/desktop.module.scss";
-import mobileStyles from "./../../styles/components/page_layout/footers/mobile.module.scss";
-
-// desktop component
-
-// const DesktopFooter = () => {
-//   return (
-//     <footer id={desktopStyles.footer_container}>
-//       <div id={desktopStyles.description_wrapper}>
-//         <Image
-//           id={desktopStyles.logo}
-//           src={"/logo-dark.png"}
-//           width={200}
-//           height={200}
-//           alt={"logo"}
-//         />
-//         <p id={desktopStyles.description}>
-//           CodeDevils is an inclusive virtual club that aims to bring ASU
-//           students together.
-//         </p>
-//       </div>
-
-//       <div id={desktopStyles.linkWrapper}>
-//         {/* <section className={desktopStyles.section_container}> */}
-//         <h2 className={desktopStyles.section_title}>Organization</h2>
-//         <ul className={desktopStyles.link_list}>
-//           <li className={desktopStyles.list_item}>
-//             <Link href={"/about"}>About</Link>
-//           </li>
-//           <li className={desktopStyles.list_item}>
-//             <Link href={"/about#leadership"}>Leadership</Link>
-//           </li>
-//           <li className={desktopStyles.list_item}>
-//             <Link
-//               target="_blank"
-//               href={
-//                 "https://asu.campuslabs.com/engage/organization/codedevils/"
-//               }
-//             >
-//               SunDevilSync
-//             </Link>
-//           </li>
-//           <li className={desktopStyles.list_item}>
-//             <Link target="_blank" href={"https://github.com/ASU-CodeDevils/"}>
-//               Github
-//             </Link>
-//           </li>
-//           <li className={desktopStyles.list_item}>
-//             <Link
-//               target="_blank"
-//               href={"https://www.hackerrank.com/codedevils-spring-challenge"}
-//             >
-//               HackerRank
-//             </Link>
-//           </li>
-
-//           <li className={desktopStyles.list_item}>
-//             <Link
-//               target="_blank"
-//               href={"https://app.slack.com/client/T2N76FZ3Q/C2N5P84BD"}
-//             >
-//               Slack
-//             </Link>
-//           </li>
-//         </ul>
-//         {/* </section> */}
-//       </div>
-//     </footer>
-//   );
-// };
-
-// const MobileFooter = () => {
-//   const openPDF = (path) => {
-//     window.open(path, "_blank");
-//   };
-
-//   const { ref: footerRef, inView: footerInView } = useInView({
-//     threshold: 0.5,
-//     triggerOnce: true,
-//   });
-
-//   return (
-//     <footer id={mobileStyles.footer_container}>
-//       <div id={mobileStyles.description_wrapper}>
-//         <Image
-//           src="/logo-dark.png"
-//           alt="CodeDevils logo"
-//           width={150}
-//           height={200}
-//         />
-//         <p id={mobileStyles.description}>
-//           CodeDevils is an inclusive virtual club that aims to bring ASU
-//           students together.
-//         </p>
-//       </div>
-//       <div id={mobileStyles.section_wrapper}>
-//         <h3 id={mobileStyles.section_title}>Organization</h3>
-//         <ul id={mobileStyles.section_list}>
-//           <li className={mobileStyles.section_list_item}>
-//             <Link href="/about">About</Link>
-//           </li>
-//           <li className={mobileStyles.section_list_item}>
-//             <Link href="/about/#leadership">Leadership</Link>
-//           </li>
-//           <li className={mobileStyles.section_list_item}>
-//             <Link
-//               target="_blank"
-//               href="https://asu.campuslabs.com/engage/organization/codedevils/"
-//             >
-//               SunDevilSync
-//             </Link>
-//           </li>
-//           <li className={mobileStyles.section_list_item}>
-//             <button onClick={() => openPDF("/pdf/welcome.pdf")}>
-//               Welcome Docs
-//             </button>
-//           </li>
-//           <li className={mobileStyles.section_list_item}>
-//             <Link href="/about/#leadership">Constitution</Link>
-//           </li>
-//           <li className={mobileStyles.section_list_item}>
-//             <Link href="/about/#leadership">Slack</Link>
-//           </li>
-//         </ul>
-//       </div>
-//     </footer>
-//   );
-// };
+import { AiOutlineInstagram, AiOutlineLink } from "react-icons/ai";
+import { FaDiscord, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   const openPDF = (path) => {
@@ -146,74 +17,155 @@ const Footer = () => {
   }, [currentYear]);
 
   return (
-    <footer className="flex flex-col flex-nowrap items-center justify-center py-5 px-2 shadow-lg">
-      <section className="flex flex-col flex-nowrap items-center justify-center">
-        <Image
-          src="/logo-dark.png"
-          alt="CodeDevils logo"
-          width={150}
-          height={200}
-        />
-        <p className="text-center">
-          CodeDevils is an inclusive virtual club that aims to bring ASU
-          students together.
-        </p>
-      </section>
-
-      <ul>
-        <li>
-          <h3 className="text-center font-bold">Organization</h3>
-          <ul className="text-center">
+    <footer className="bg-white dark:bg-gray-900">
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 md:mb-0">
+            <Link href="/" className="flex items-center">
+              <Image
+                src="/logo-dark.png"
+                alt="CodeDevils logo"
+                width={150}
+                height={200}
+              />
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <h2 className="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
+                Organization
+              </h2>
+              <ul className="font-medium text-black">
+                {[
+                  ["About", "/about"],
+                  ["Leadership", "/about/#leadership"],
+                  ["Contact", "/contact"],
+                  ["Constitution", "/about/#constitution"],
+                ].map(([title, url], key) => (
+                  <li key={key} className="mb-4">
+                    <Link
+                      href={url as string}
+                      className="hover:text-maroon hover:underline"
+                    >
+                      {title}
+                    </Link>
+                  </li>
+                ))}
+                <li>
+                  <button
+                    className="hover:underline"
+                    onClick={() => openPDF("/pdf/welcome.pdf")}
+                  >
+                    Welcome Docs
+                  </button>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
+                Follow us
+              </h2>
+              <ul className="font-medium text-black">
+                {[
+                  ["Instagram", "https://www.instagram.com/asu_codedevils/"],
+                  ["Discord Community", "https://discord.gg/9YCuQ5B48v"],
+                  [
+                    "SunDevilSync",
+                    "https://asu.campuslabs.com/engage/organization/codedevils/",
+                  ],
+                  ["Github", "https://github.com/ASU-CodeDevils/"],
+                  [
+                    "LinkedIn",
+                    "https://www.linkedin.com/company/42138997/admin/feed/posts/",
+                  ],
+                ].map(([title, url], key) => (
+                  <li key={key} className="mb-4 last:mb-0">
+                    <Link
+                      className="hover:text-maroon hover:underline"
+                      target="_blank"
+                      href={url as string}
+                    >
+                      {title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
+                Legal
+              </h2>
+              <ul className="font-medium text-black">
+                {[
+                  ["Privacy Policy", "#"],
+                  ["Terms & Conditions", "#"],
+                ].map(([title, url], key) => (
+                  <li key={key} className="mb-4 last:mb-0">
+                    <Link
+                      className="hover:text-maroon hover:underline"
+                      href={url as string}
+                    >
+                      {title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+        <hr className="my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <span className="text-sm text-black">
+            © 2023{" "}
+            <Link
+              href="/"
+              className="text-marron transition-colors duration-200 ease-in-out hover:text-maroon md:text-black"
+            >
+              CodeDevils
+            </Link>
+            . All Rights Reserved.
+          </span>
+          <ul className="mt-4 flex space-x-5 sm:mt-0 sm:justify-center">
             {[
-              ["About", "/about"],
-              ["Leadership", "/about/#leadership"],
-              ["Contact", "/contact"],
-              ["Constitution", "/about/#constitution"],
-            ].map(([title, url], key) => (
-              <li key={key}>
-                <Link href={url as string}>{title}</Link>
-              </li>
-            ))}
-            <li>
-              <button onClick={() => openPDF("/pdf/welcome.pdf")}>
-                Welcome Docs
-              </button>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <h3 className="text-center font-bold">Follow</h3>
-          <ul className="text-center">
-            {[
-              ["Instagram", "https://www.instagram.com/asu_codedevils/"],
-              ["Discord Community", "https://discord.gg/9YCuQ5B48v"],
               [
+                <AiOutlineInstagram key={1} />,
+                "Instagram",
+                "https://www.instagram.com/asu_codedevils/",
+              ],
+              [
+                <FaDiscord key={2} />,
+                "Discord",
+                "https://discord.gg/9YCuQ5B48v",
+              ],
+              [
+                <AiOutlineLink key={3} />,
                 "SunDevilSync",
                 "https://asu.campuslabs.com/engage/organization/codedevils/",
               ],
-              ["Github", "https://github.com/ASU-CodeDevils/"],
               [
+                <FaGithub key={4} />,
+                "Github",
+                "https://github.com/ASU-CodeDevils/",
+              ],
+              [
+                <FaLinkedin key={5} />,
                 "LinkedIn",
                 "https://www.linkedin.com/company/42138997/admin/feed/posts/",
               ],
-            ].map(([title, url], key) => (
+            ].map(([icon, title, url], key) => (
               <li key={key}>
-                <Link target="_blank" href={url as string}>
-                  {title}
+                <Link
+                  className="text-black transition-colors duration-200 ease-in-out hover:text-maroon dark:hover:text-white"
+                  href={url as string}
+                >
+                  <span className="h-4 w-4">{icon}</span>
+                  <span className="sr-only">{title}</span>
                 </Link>
               </li>
             ))}
           </ul>
-        </li>
-      </ul>
-
-      <section></section>
-      <section>
-        <p className="after:content-['All Rights Reserved'] before:content-['@']">
-          {" "}
-          Copyright {currentYear}.
-        </p>
-      </section>
+        </div>
+      </div>
     </footer>
   );
 };
