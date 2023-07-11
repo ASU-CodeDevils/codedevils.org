62a7c89d9a8b234e29af85c1ed07274dbc5d9f1e
David Nevarez <david.nevarez03@hotmail.com>
2023-07-10 21:19:44 -0700

    replaced SessionProvider with ClerkProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,18 @@
-import type { Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
 import type { AppType } from "next/app";
 import Layout from "../components/Layout";
 import { trpc } from "../utils/trpc";
 import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
 // gobal styles
 import "./../styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   return (
-    <SessionProvider session={session}>
+    <ClerkProvider {...pageProps}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </SessionProvider>
+    </ClerkProvider>
   );
 };
 
