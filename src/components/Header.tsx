e462f41527860341f133b32d25a24b7edee88028
David Nevarez <david.nevarez03@hotmail.com>
2023-07-18 16:25:02 -0700

    Updated custom header by revamping the whole component.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,129 +1,34 @@
 import React from "react";
-import { useRouter } from "next/router";
-
-import Typewriter from "typewriter-effect";
 import { useInView } from "react-intersection-observer";
-
-// wave divider
 import Divider from "./Divider";
 
-// font optimization
-import { Poppins } from "@next/font/google";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400"],
-});
-
-type NonHomeHeaderProps = {
-  url: string;
-  title: string;
-  description: string;
-  key: number;
+type HeaderProps = {
+  title?: string | undefined;
+  description?: string | undefined;
 };
 
-const NonHomeHeader = ({
-  url,
-  title,
-  description,
-  key,
-}: NonHomeHeaderProps) => {
-  const { asPath } = useRouter();
-
-  const { ref: myRef, inView: isInView } = useInView({
-    triggerOnce: false,
-    // threshold: 0,
-  });
-  return (
-    <div
-      key={key}
-      ref={myRef}
-      className={`${
-        asPath === url
-          ? "flex flex-col"
-          : {
-              // hid the header, then show it again
-            }
-      } ${
-        isInView
-          ? "translate-x-0	opacity-100 blur-0"
-          : "-translate-x-full opacity-0 blur"
-      } w-11/12 px-8 pt-0 pb-10 text-white transition-all duration-500 md:w-6/12`}
-    >
-      <h1 className="text-[3.5rem] font-light">{title}</h1>
-      <p className="text-[1rem] font-light md:text-[1.25rem]">{description}</p>
-    </div>
-  );
-};
-
-const Header = () => {
-  const { asPath } = useRouter();
-
+const Header = ({ title, description }: HeaderProps) => {
   const { ref: myRef, inView: isInView } = useInView({
     triggerOnce: false,
   });
 
   return (
-    <header className="bg-hero h-fit w-screen bg-cover pt-[120px] text-white">
-      {asPath === "/" ? (
-        <div
-          ref={myRef}
-          className={`${
-            isInView
-              ? "translate-x-0	opacity-100 blur-0"
-              : "-translate-x-full opacity-0 blur"
-          } items-left flex w-8/12 flex-col flex-wrap justify-start px-4 pb-12 text-[3.5rem] font-light transition-all duration-1000`}
-        >
-          <p className="font-light">
-            <strong className={`${poppins.className} font-extrabold`}>
-              CodeDevils
-            </strong>{" "}
-            - An ASU Student Organization
-          </p>
-          <Typewriter
-            options={{
-              strings: ["Develop.", "Create.", "Network."],
-              autoStart: true,
-              delay: 50,
-              cursor: "|",
-              loop: true,
-              // for styling the typewriter wrapper
-              wrapperClassName: "font-extrabold",
-              // for styling the cursor
-              // NOTE: this will remove the default cursor style, which include the blinking. So make sure to add that back in.
-              // cursorClassName: 'typewriter__cursor',
-            }}
-          />
-        </div>
-      ) : (
-        <>
-          {[
-            [
-              "/about",
-              "About",
-              "CodeDevils aims to provide ASU students with an organization designed to encourage and promote involvement in coding projects and competitions through an exclusively online setting.",
-            ],
-            [
-              "/contact",
-              "Contact Us",
-              "Get in touch and let us know how we can help you with your coding needs. Fill out the form and we'll be in touch as soon as possible.",
-            ],
-          ].map(
-            ([url, title, description], key) =>
-              asPath === url && (
-                <NonHomeHeader
-                  url={url as string}
-                  title={title as string}
-                  description={description as string}
-                  key={key as number}
-                />
-              )
-          )}
-          <Divider />
-        </>
-      )}
+    <header className="bg-hero w-screen bg-cover pt-20 text-white">
+      <div
+        ref={myRef}
+        className={`${
+          isInView
+            ? "translate-x-0	opacity-100 blur-0"
+            : "-translate-x-full opacity-0 blur"
+        } w-screen py-4 pl-10 text-[3rem] transition-all duration-1000 ease-in-out md:w-7/12`}
+      >
+        {title && <h1 className="text-left font-light">{title}</h1>}
+        {description && (
+          <p className="text-left text-[1.25rem] font-light">{description}</p>
+        )}
+      </div>
 
-      {/* {asPath !== "/" && <Divider />} */}
+      <Divider />
     </header>
   );
 };
