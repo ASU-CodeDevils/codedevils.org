3dcdae2d92e430e26393771f77dc1b4841ee1a51
David Nevarez <david.nevarez03@hotmail.com>
2023-07-18 16:32:41 -0700

    Updated the navbar to work with the design when the user is logged in.
    Added new icons for the logged-in settings icon, updated login and register routing, and now the component is using the SettingsBtn component..

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,16 +2,22 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
+import { useUser } from "@clerk/nextjs";
+
 import { FaLanguage } from "react-icons/fa";
 import { HiUserAdd } from "react-icons/hi";
 import { FiLogIn } from "react-icons/fi";
-
+import { AiOutlineUser } from "react-icons/ai";
 import { GiHamburgerMenu } from "react-icons/gi";
+
+import SettingsBtn from "./SettingsBtn";
 
 const Navbar = () => {
   const [isScrolling, setIsScrolling] = useState(false);
   const [showDropdown, setShowDropdown] = useState(false);
   const [showMobileNav, setShowMobileNav] = useState(false);
+
+  const { isSignedIn } = useUser();
 
   useEffect(() => {
     // Add a listener to the window scroll event
@@ -23,7 +29,7 @@ const Navbar = () => {
     };
   }, []);
 
-  const handleScroll = () => setIsScrolling(window.scrollY > 60);
+  const handleScroll = () => setIsScrolling(window.scrollY > 40); // 40px === 2.5rem
 
   type StyleType = {
     scrollStyles?: string | undefined;
@@ -35,7 +41,7 @@ const Navbar = () => {
     <nav
       className={`${showMobileNav && "bg-white"} ${
         isScrolling ? "bg-white md:shadow" : "md:bg-transparent"
-      } md:min-h-{5.563rem} fixed top-0 left-0 z-10 w-screen min-w-fit py-2 md:fixed md:top-0 md:left-0 md:z-50 md:flex md:flex-nowrap md:items-center md:justify-between md:px-12 md:py-1 md:transition-all md:duration-1000 md:ease-in-out`}
+      } md:min-h-{5.563rem} fixed top-0 left-0 z-10 w-screen min-w-fit py-2 md:fixed md:top-0 md:left-0 md:z-50 md:flex md:flex-nowrap md:items-center md:px-12 md:py-1 md:transition-all md:duration-1000 md:ease-in-out`}
     >
       <Link
         onClick={() => setShowMobileNav(false)}
@@ -53,13 +59,14 @@ const Navbar = () => {
       <button
         className={`fixed top-7 right-3 flex  rounded border-solid p-[.6rem] text-xl outline-0 transition-colors duration-300 ease-in-out hover:bg-gray-600 focus:border-[.1rem] focus:border-gray-400 focus:p-2 md:hidden ${
           showMobileNav
-            ? "text-maroon"
+            ? "text-black"
             : ` hover:text-maroon focus:text-white ${
                 isScrolling ? "text-maroon" : "text-white"
               }`
         }`}
         onClick={() => setShowMobileNav(!showMobileNav)}
       >
+        {/* add styles to this icon */}
         <GiHamburgerMenu />
       </button>
 
@@ -104,96 +111,115 @@ const Navbar = () => {
         ))}
       </ul>
 
-      <ul
-        className={`${
-          showMobileNav && "flex text-black md:relative md:top-0 md:right-0"
-        } w-[30vw] md:my-0 md:mx-[0.25rem] md:flex md:min-w-fit md:flex-[1] md:justify-end md:transition-all md:duration-300 md:ease-out`}
-      >
-        {[
-          [
-            "Register",
-            "/accounts/register",
-            <HiUserAdd key={1} />,
-            {
-              scrollStyles:
-                "border-maroon bg-white text-maroon hover:bg-maroon hover:text-white",
-              nonScrollStyles: "text-white hover:bg-white hover:text-black",
-              customGeneralStyles: `${"hidden md:flex"}`,
-            },
-          ],
-          [
-            "Login",
-            "/accounts/login",
-            <FiLogIn key={2} />,
-            {
-              scrollStyles: `md:border-maroon bg-maroon text-white md:hover:border-black md:hover:bg-black md:hover:text-white md:hover:shadow-lg`,
-              nonScrollStyles: "bg-white text-black md:hover:bg-gray-600",
-              customGeneralStyles: `${`absolute top-[1.7rem] right-[17vw]  z-50 md:relative md:top-0 md:right-0`}`,
-            },
-          ],
-        ].map(([title, url, icon, styles], key) => {
-          const { scrollStyles, nonScrollStyles, customGeneralStyles } =
-            styles as StyleType;
-          return (
-            <li
-              key={key}
-              className={`md:font-["Space Grotesk"] flex flex-nowrap items-center rounded-3xl uppercase md:mx-[.25rem] md:border md:border-solid md:p-2 md:text-[0.75rem] md:tracking-[1.45px] md:transition-all md:duration-200 md:ease-in-out ${customGeneralStyles}  ${
-                isScrolling ? scrollStyles : nonScrollStyles
-              } `}
-            >
-              <div className={`hidden md:flex`}>
-                {/* hide icons on mobile */}
-                {icon as React.ReactNode}
-              </div>
-              <Link
-                onClick={() => setShowMobileNav(false)}
-                href={url as string}
-                className={`rounded-xl p-[.7rem] md:ml-2 md:rounded-none md:bg-transparent md:p-0 ${
-                  isScrolling ? "bg-maroon" : "bg-white"
-                }`}
+      {!isSignedIn && (
+        <ul
+          className={`${
+            showMobileNav && "flex text-black md:relative md:top-0 md:right-0"
+          } w-[30vw] md:my-0 md:mx-[0.25rem] md:flex md:min-w-fit md:flex-[1] md:justify-end md:transition-all md:duration-300 md:ease-out`}
+        >
+          {[
+            [
+              "Register",
+              "/register",
+              <HiUserAdd key={1} />,
+              {
+                scrollStyles:
+                  "border-maroon bg-white text-maroon hover:bg-maroon hover:text-white",
+                nonScrollStyles: "text-white hover:bg-white hover:text-black",
+                customGeneralStyles: `${"hidden md:flex"}`,
+              },
+            ],
+            [
+              "Login",
+              "/login",
+              <FiLogIn key={2} />,
+              {
+                scrollStyles: `md:border-maroon bg-maroon text-white md:hover:border-black md:hover:bg-black md:hover:text-white md:hover:shadow-lg`,
+                nonScrollStyles: "bg-white text-black md:hover:bg-gray-600",
+                customGeneralStyles: `${`absolute top-[1.7rem] right-[17vw]  z-50 md:relative md:top-0 md:right-0`}`,
+              },
+            ],
+          ].map(([title, url, icon, styles], key) => {
+            const { scrollStyles, nonScrollStyles, customGeneralStyles } =
+              styles as StyleType;
+            return (
+              <li
+                key={key}
+                className={`md:font-["Space Grotesk"] flex flex-nowrap items-center rounded-3xl uppercase md:mx-[.25rem] md:border md:border-solid md:p-2 md:text-[0.75rem] md:tracking-[1.45px] md:transition-all md:duration-200 md:ease-in-out ${customGeneralStyles}  ${
+                  isScrolling ? scrollStyles : nonScrollStyles
+                } `}
               >
-                {title as string}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+                <div className={`hidden md:flex`}>
+                  {/* hide icons on mobile */}
+                  {icon as React.ReactNode}
+                </div>
+                <Link
+                  onClick={() => setShowMobileNav(false)}
+                  href={url as string}
+                  className={`rounded-xl p-[.7rem] md:ml-2 md:rounded-none md:bg-transparent md:p-0 ${
+                    isScrolling ? "bg-maroon" : "bg-white"
+                  }`}
+                >
+                  {title as string}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
 
       <div
         className={`${
           showMobileNav ? "flex" : "hidden md:flex"
-        } items-center justify-center rounded-full p-[.7rem] shadow-xl transition-all duration-200 ease-in-out ${
+        } ml-auto items-center justify-center rounded-full p-[.7rem] shadow-xl transition-all duration-200 ease-in-out ${
           isScrolling
-            ? "bg-maroon text-white hover:bg-black"
-            : "bg-white hover:bg-gray-600 dark:bg-black dark:hover:bg-gray-100"
+            ? "bg-maroon hover:bg-black"
+            : "bg-white hover:bg-gray-600  dark:hover:bg-gray-100"
         }`}
         onMouseOver={() => setShowDropdown(true)}
         onMouseLeave={() => setShowDropdown(false)}
       >
-        <FaLanguage />
-
-        <ul
-          className={`${
-            showDropdown
-              ? "absolute right-[1rem] top-[4.5rem] flex h-auto min-w-[2em] flex-col flex-nowrap bg-white p-[20px] shadow-md transition-all duration-300 ease-in-out before:fixed before:top-[3.75rem] before:right-[3.5rem] before:h-0 before:w-0 before:border-[10px] before:border-t-0 before:border-b-[20px] before:border-solid before:border-transparent before:border-b-white"
-              : "hidden"
-          }`}
-        >
-          {[
-            "Española",
-            "English",
-            "Française",
-            "عربى",
-            "Nederlands",
-            "Deutsche",
-            "日本語",
-            "हिन्दी",
-          ].map((language, key) => (
-            <li key={key}>
-              <button className="text-black ">{language}</button>
-            </li>
-          ))}
-        </ul>
+        {isSignedIn ? (
+          <>
+            <SettingsBtn
+              showDropdown={showDropdown}
+              icon={
+                <AiOutlineUser
+                  className={`${isScrolling ? "text-white" : "text-black"}`}
+                />
+              }
+              list={
+                [
+                  ["Profile", "/profile"],
+                  ["Settings", "/settings"],
+                ] as [string, string][]
+              }
+            />
+          </>
+        ) : (
+          <>
+            <SettingsBtn
+              showDropdown={showDropdown}
+              icon={
+                <FaLanguage
+                  className={`${isScrolling ? "text-white" : "text-black"}`}
+                />
+              }
+              list={
+                [
+                  "Española",
+                  "English",
+                  "Française",
+                  "عربى",
+                  "Nederlands",
+                  "Deutsche",
+                  "日本語",
+                  "हिन्दी",
+                ] as string[]
+              }
+            />
+          </>
+        )}
       </div>
     </nav>
   );
