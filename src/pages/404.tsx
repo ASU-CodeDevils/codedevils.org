003d5952566232b7d3eba812cc121ae537799633
David Nevarez <david.nevarez03@hotmail.com>
2023-07-19 16:40:24 -0700

    Added custom header component to the page.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { NextPage } from "next";
+import Header from "~/components/Header";
 
 const ErrorPage: NextPage = () => {
   return (
     <>
+      <Header />
       <h1 className="bold text-[3rem]">404 Error</h1>
       <p>
         Oops! Something went wrong. Seems like this isn&#180;t the page you are
