dca1700527ab7712e8a4e356ca6c50d15e66e4a6
David Nevarez <69279785+SloppyFlipFlop@users.noreply.github.com>
2024-01-19 10:04:50 -0700

    Created page to display when a 404 error occurs.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,27 @@
 import React from "react";
-import { NextPage } from "next";
 import Header from "~/components/Header";
+import Link from "next/link";
+import { FaArrowRight } from "react-icons/fa";
 
-const ErrorPage: NextPage = () => {
+export default function Custom404() {
   return (
     <>
-      <Header />
-      <h1 className="bold text-[3rem]">404 Error</h1>
-      <p>
-        Oops! Something went wrong. Seems like this isn&#180;t the page you are
-        looking for.
-      </p>
+      <div className="isolate">
+        <Header>
+          <h1 className="text-3xl text-7xl font-bold">404 Error</h1>
+          <article className="space-y-4">
+            <p className="text-base md:w-1/2">
+              Oops! Something went wrong. Seems like this isn&#180;t the page
+              you are looking for.
+            </p>
+            <p className="text-base">
+              <Link className=" text-white" href="/">
+                Go to the Home Page <FaArrowRight className="inline" />
+              </Link>
+            </p>
+          </article>
+        </Header>
+      </div>
     </>
   );
-};
-
-export default ErrorPage;
+}
