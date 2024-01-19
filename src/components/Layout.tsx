bc1c8dfd2e99c53db4f5377dc6b61606475da5b6
David Nevarez <69279785+SloppyFlipFlop@users.noreply.github.com>
2024-01-19 10:24:47 -0700

    Updated website's language setting to improve SEO accessibility.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -41,7 +41,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         />
         <meta name="author" content="" />{" "}
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="icon" href="/favicon.webp" />{" "}
+        <link rel="icon" href="/favicon.webp" />
+        <html lang="en" />
       </Head>
 
       <div className="flex h-screen flex-col ">
