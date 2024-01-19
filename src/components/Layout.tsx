464c0cf1a49bbb5598921b60a34b0401938d165d
David Nevarez <69279785+SloppyFlipFlop@users.noreply.github.com>
2024-01-19 10:10:41 -0700

    Updated layout component to dynamically load the footer. Added a custom logging message as an Easter egg for users viewing the console. Updated the website's head with a new favicon image format and title.
    Left comments throughout the code specifying the font to use on the website, as I plan to revisit and address the issue with @next/font.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,39 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 
-import Banner from "./Banner";
 import Navbar from "./Navbar";
-import Footer from "./Footer";
 
-import { Inter } from "@next/font/google";
+// import { Roboto } from "@next/font/google";
 
-// If loading a variable font, you don't need to specify the font weight
-const inter = Inter({ subsets: ["latin"] });
+// const roboto = Roboto({
+//   weight: ["400", "500", "700"],
+//   subsets: ["latin"],
+// });
+
+import dynamic from "next/dynamic";
+
+const DynamicFooter = dynamic(() => import("./Footer"), {
+  loading: () => <p>Loading...</p>,
+});
+
+// ${roboto.className}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    console.log(
+      `
+      Intrested in learning about Web Dev or wantig to gain more project experience,
+      consider contributing to this website: https://github.com/ASU-CodeDevils/codedevils.org.
+      `
+    );
+  }, []);
   return (
     <div
-      className={`${inter.className} mx-auto flex h-screen flex-col flex-nowrap bg-white text-black `}
+      className={` mx-auto flex h-screen flex-col flex-nowrap bg-white text-black
+    
+       `}
     >
       <Head>
-        <title>CodeDevils - Develop. Network. Create.</title>
+        <title>CodeDevils - Develop, Create, and Network</title>
         <meta
           name="description"
           content="CodeDevils is an inclusive virtual club that aims to bring ASU students together."
         />
         <meta name="author" content="" />{" "}
-        {/* TODO: add all the developers that worked on the website at the end */}
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="icon" href="/favicon.png" />{" "}
+        <link rel="icon" href="/favicon.webp" />{" "}
       </Head>
 
       <div className="flex h-screen flex-col ">
         <Navbar />
+
         <main className="mx-auto mt-20 w-full max-w-screen-2xl md:mt-16">
           {children}
         </main>
 
-        <Footer />
+        <DynamicFooter />
       </div>
     </div>
   );
