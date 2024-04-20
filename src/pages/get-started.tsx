2282dd5f756d81addf5c0627b8a5d5d9759f81aa
David Nevarez <69279785+SloppyFlipFlop@users.noreply.github.com>
2024-04-20 04:30:24 -0700

    feature: Updated entire page content

---
+++ src/pages/get-started.tsx
@@ -0,0 +1,133 @@
+import { NextPage } from "next";
+import { Hero, Section, SupportSection } from "~/components/ui/sections";
+import { ExternalLink } from "~/components/ui/buttons";
+import { socialLinks } from "~/utils/staticdata";
+
+const getStarted: NextPage = () => {
+  const { discord, sundevilsync, github } = socialLinks;
+  return (
+    <>
+      <Hero className="bg-gray">
+        <article className="space-y-3 text-center">
+          <h3 className="gradient-text icon-sparkle rainbow text-3xl font-medium md:text-4xl">
+            Community
+          </h3>
+          <h2 className="mx-auto text-5xl font-bold md:w-2/3 md:text-6xl">
+            Welcome to the CodeDevils&apos; Community
+          </h2>
+          <p className="text-xl text-black/70 md:mx-auto md:w-7/12">
+            Learn from and connect with other developers.
+          </p>
+        </article>
+      </Hero>
+
+      <Section id="community">
+        <article className="space-y-3 text-center">
+          <h3 className="text-xl text-maroon">Get involved</h3>
+          <h2 className="text-4xl font-bold">
+            Engage with our thriving community
+          </h2>
+          <p className="mx-auto w-9/12 text-base">
+            We offer a rich online community where members can find their place
+            with us. You can engage in insightful discussions, share knowledge,
+            collaborate on projects, and create valuable connections with other
+            members.
+          </p>
+
+          <ExternalLink href={discord} className="mx-auto">
+            Join our community
+          </ExternalLink>
+        </article>
+
+        <ul className="max-w-screeen-md mx-auto grid gap-y-4 md:grid-cols-3 md:gap-x-4">
+          <li className="space-y-4 rounded-xl bg-white p-6 shadow-xl">
+            <div className="space-y-1">
+              <h4 className="text-xl font-medium">SunDevilSync</h4>
+              <p>
+                Register with us on SunDevilSync to stay updated with our events
+                and activities.
+              </p>
+            </div>
+
+            <ExternalLink href={sundevilsync}>Register with us</ExternalLink>
+          </li>
+
+          <li className="space-y-4 rounded-xl bg-white p-6 shadow-xl">
+            <div className="space-y-1">
+              <h4 className="text-xl font-medium">Github</h4>
+              <p>Join our Github organization to contribute to our projects</p>
+            </div>
+
+            <ExternalLink href={github}>Join us on Github</ExternalLink>
+          </li>
+
+          <li className="space-y-4 rounded-xl bg-white p-6 shadow-xl">
+            <div className="space-y-1">
+              <h4 className="text-xl font-medium">Newsletter</h4>
+              <p>
+                Subscribe to our newsletter to stay updated with our latest
+                news.
+              </p>
+            </div>
+
+            <ExternalLink href="https://forms.gle/hkqP2ZMuDrppxNsr8">
+              Subscribe now
+            </ExternalLink>
+          </li>
+        </ul>
+      </Section>
+
+      <Section>
+        <article className="space-y-3 text-center">
+          <h3 className="text-xl text-maroon">Grow your skills</h3>
+          <h2 className="text-4xl font-bold">
+            Learn and improve your skills with our resources
+          </h2>
+          <p className="mx-auto text-base md:w-9/12">
+            We offer a wide range of resources to help you grow your skills as a
+            software developer. From courses to workshops, projects, and
+            competitions, you&apos;ll find everything you need to succeed in
+            your software development journey.
+          </p>
+        </article>
+
+        <ul className="mx-auto grid max-w-screen-lg gap-6 md:grid-cols-2">
+          <li className="space-y-2 rounded-lg bg-white p-6 shadow-2xl">
+            <h4 className="text-2xl">Courses</h4>
+            <p className="text-base">
+              Get access to free courses on various topics to help you grow your
+              skills and become the best verison of yourself as a developer.
+            </p>
+          </li>
+          <li className="space-y-2 rounded-lg bg-white p-6 shadow-2xl">
+            <h4 className="text-2xl">Workshops</h4>
+            <p className="text-base">
+              Attend workshops to learn new technologies and tools from industry
+              professionals.
+            </p>
+          </li>
+
+          <li className="space-y-2 rounded-lg bg-white p-6 shadow-2xl">
+            <h4 className="text-2xl">Projects</h4>
+            <p className="text-base">
+              Take part in real-world projects to gain experience and build your
+              portfolio up for future job opportunities.
+            </p>
+          </li>
+
+          <li className="space-y-2 rounded-lg bg-white p-6 shadow-2xl">
+            <h4 className="text-2xl">Competitions</h4>
+            <p className="text-base">
+              Compete in our coding competitions to test your skills and win
+              prizes.
+            </p>
+          </li>
+        </ul>
+      </Section>
+
+      <SupportSection />
+    </>
+  );
+};
+
+export default getStarted;
---
+++ src/pages/projects.tsx
@@ -0,0 +1,232 @@
+import React from "react";
+import { NextPage } from "next";
+
+import Image from "next/image";
+
+import { Card } from "../components/Card";
+import { Section, SupportSection } from "~/components/ui/sections";
+import { ExternalLink, InternalLink } from "~/components/ui/buttons";
+
+import { socialLinks } from "~/utils/staticdata";
+
+const projectsPage: NextPage = () => {
+  const { discord } = socialLinks;
+  return (
+    <>
+      <Section>
+        <article className="space-y-3 text-center">
+          <h3 className="text-3xl text-maroon">Our Projects</h3>
+          <h2 className="mx-auto text-5xl font-bold md:w-2/3 md:text-6xl">
+            Grow your resume with our open-source projects
+          </h2>
+          <p className="text-xl text-black/70">
+            Learn and improve your skills by contributing to our projects.
+          </p>
+        </article>
+
+        {/* Card */}
+        <div
+          className="relative flex h-[35rem] items-center justify-center md:h-[50rem]" // md:h-[80rem]
+        >
+          <div
+            className="relative w-full py-10 md:py-28"
+            style={{
+              perspective: "1000px",
+            }}
+          >
+            <Card>
+              <Image
+                src={`/images/github.png`}
+                alt="hero"
+                height={720}
+                width={1400}
+                className="mx-auto hidden h-full rounded-2xl object-cover object-left-top md:block"
+                draggable={false}
+              />
+
+              <Image
+                src={`/images/github-mobile.png`}
+                alt="hero"
+                height={1280}
+                width={1400}
+                className="mx-auto block h-full rounded-2xl object-cover object-left-top md:hidden"
+                draggable={false}
+              />
+            </Card>
+          </div>
+        </div>
+      </Section>
+
+      <Section className="bg-gray">
+        <article className="space-y-3 text-center">
+          <h3 className="text-2xl text-maroon">Get involved</h3>
+          <h2 className="text-4xl font-bold">
+            Contribute to our open-source projects
+          </h2>
+          <p className="mx-auto w-9/12 text-base">
+            Get involved with our open-source projects to build your portfolio
+            up for future job opportunities. We have a variety of projects that
+            you can contribute to help you grow your skills and gain real-world
+            experience.
+          </p>
+        </article>
+
+        <ul className="mx-auto flex max-w-screen-lg flex-col gap-y-8 md:grid md:grid-cols-3 md:gap-x-8">
+          <li className="flex flex-col justify-between space-y-4 rounded-lg bg-white px-6 py-12 text-center shadow-xl">
+            <div className="space-y-2">
+              <h4 className="text-xl font-medium">CodeDevils&apos; Website</h4>
+              <p className="text-base text-black/90">
+                The official CodeDevils&apos; website that you are currently
+                viewing.
+              </p>
+            </div>
+            <ExternalLink
+              href="https://github.com/ASU-CodeDevils/codedevils.org"
+              className="mx-auto"
+            >
+              View on GitHub
+            </ExternalLink>
+          </li>
+
+          <li className="flex flex-col justify-between space-y-4 rounded-lg bg-white px-6 py-12 text-center shadow-xl">
+            <div className="space-y-2">
+              <h4 className="text-xl font-medium">DevilBot</h4>
+              <p className="text-base text-black/90">
+                A Bot written in Rust to provide utility and fun commands within
+                our Discord server.
+              </p>
+            </div>
+            <ExternalLink
+              href="https://github.com/ASU-CodeDevils/discordo-rusto-boto"
+              className="mx-auto"
+            >
+              View on GitHub
+            </ExternalLink>
+          </li>
+
+          <li className="flex flex-col justify-between space-y-4 rounded-lg bg-white px-6 py-12 text-center shadow-xl">
+            <div className="space-y-2">
+              <h4 className="text-xl font-medium">
+                Our React Components Library
+              </h4>
+              <p className="text-base text-black/90">
+                A Components Library that is used across all of our projects to
+                maintain a consistent design system.
+              </p>
+            </div>
+            <ExternalLink
+              href="https://github.com/ASU-CodeDevils/CD-Library"
+              className="mx-auto"
+            >
+              View on GitHub
+            </ExternalLink>
+          </li>
+        </ul>
+      </Section>
+
+      <Section>
+        <article className="space-y-3 text-center">
+          <h3 className="text-2xl text-maroon">Get Started</h3>
+          <h2 className="text-4xl font-bold">
+            Want to contribute to our projects, but you&apos;re new?
+          </h2>
+          <p className="mx-auto w-9/12 text-base">
+            No worries! We have a guide to help you get started with
+            contributing to our projects. We welcome all skill levels to
+            contribute to our projects.
+          </p>
+        </article>
+
+        <div className="mx-auto flex max-w-screen-lg flex-col gap-y-6 md:grid md:grid-cols-2 md:gap-x-12">
+          <Image
+            src="/images/YHFVJK.png"
+            alt="GitHub"
+            width={500}
+            height={500}
+            className=""
+          />
+
+          <ul className="flex flex-col gap-y-6">
+            <li className="flex flex-col gap-y-4 rounded-lg bg-gray px-4 py-6 text-base shadow-xl md:rounded-none md:bg-white md:p-0 md:shadow-none">
+              <div className="flex flex-col gap-y-2">
+                <h4 className="text-xl font-medium">
+                  Step 1: Join our Discord Server
+                </h4>
+                <p className=" text-black/90">
+                  Join our online community to stay up to date with our projects
+                  and be involved in our discussions.
+                </p>
+              </div>
+              <ExternalLink href={discord}>Join our Discord</ExternalLink>
+            </li>
+
+            <li className="flex flex-col gap-y-4 rounded-lg bg-gray px-4 py-6 text-base shadow-xl md:rounded-none md:bg-white md:p-0 md:shadow-none">
+              <div className="flex flex-col gap-y-2">
+                <h4 className="text-xl font-medium">
+                  Step 2: Talk to the project&apos;s lead
+                </h4>
+                <p className=" text-black/90">
+                  For whichever project you are interested in, talk to the lead
+                  of the project to learn how to get started.
+                </p>
+              </div>
+              <InternalLink href="/about#projectleads">
+                Meet our project leads
+              </InternalLink>
+            </li>
+
+            <li className="flex flex-col gap-y-4 rounded-lg bg-gray px-4 py-6 text-base shadow-xl md:rounded-none md:bg-white md:p-0 md:shadow-none">
+              <div className="flex flex-col gap-y-2">
+                <h4 className="text-xl font-medium">
+                  Step 3: Read our Project Documentation
+                </h4>
+                <p className=" text-black/90">
+                  Each project has its own documentation to help you get started
+                  with contributing. Make sure to read it before you start.
+                </p>
+              </div>
+              <ExternalLink href={"https://docs.codedevils.org/"}>
+                View our Documentation
+              </ExternalLink>
+            </li>
+          </ul>
+        </div>
+      </Section>
+
+      <Section>
+        <div className="flex flex-col items-center gap-6 md:grid md:grid-cols-2">
+          <article className="space-y-3 text-left">
+            <h3 className="text-2xl text-maroon">Have a project idea?</h3>
+            <h2 className="text-4xl font-bold">
+              Want to start a project with us?
+            </h2>
+            <p className="  text-base">
+              We are always looking for new project ideas to work on. If you
+              have a project idea that you want to work on with us, let us know
+              and we can help you get started.
+            </p>
+            <ExternalLink
+              href={
+                "https://codedevils.notion.site/Project-Proposal-Guide-3d0fd1768faa4afa81a880a18e0d60bc?pvs=4"
+              }
+              className=""
+            >
+              Learn more
+            </ExternalLink>
+          </article>
+          <Image
+            src="/images/WEVHIK.png"
+            alt="Project Idea"
+            width={500}
+            height={500}
+            className="rounded-lg object-cover"
+          />
+        </div>
+      </Section>
+
+      <SupportSection />
+    </>
+  );
+};
+
+export default projectsPage;
