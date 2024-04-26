68ebdcafd645aee8834918c99f3083a56c363fa8
David Nevarez <69279785+SloppyFlipFlop@users.noreply.github.com>
2024-04-26 03:52:31 -0700

    feature: Simplified header design

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,8 @@
-import React, { ReactNode } from "react";
-import Image from "next/image";
-import Link from "next/link";
+import React, { ReactNode } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
-import { cn } from "~/utils/cn";
+import { cn } from '~/utils/cn';
 
 export const Header = ({
   children,
@@ -12,26 +12,18 @@ export const Header = ({
   className?: string;
 }) => {
   return (
-    <div className="flex items-center justify-center bg-gray py-12 text-white">
-      <div className="relative">
+    <div className='flex flex-col md:grid md:grid-cols-2 md:gap-4 px-8 py-12'>
+      <div className={cn(className)}>{children}</div>
+      <div className='flex items-center'>
         <Image
-          src={"/images/YLPWQK.png"}
-          height={300}
-          width={1440}
-          alt="CodeDevils code grid header background"
-          className="hidden object-cover md:block"
+          src={'/images/YLPWQK.png'}
+          height={500}
+          width={500}
+          alt='CodeDevils code grid header background'
+          className='hidden md:block'
+          style={{ height: 'auto', width: 'auto' }}
           priority
         />
-        <div
-          className={cn(
-            "justify-left top-0 flex h-full w-full items-center px-8 text-[2rem] transition-all duration-700 md:absolute md:w-6/12 md:py-28 md:text-[3rem]",
-            className
-          )}
-        >
-          <article className="flex flex-col space-y-6 text-black">
-            {children}
-          </article>
-        </div>
       </div>
     </div>
   );
@@ -45,7 +37,7 @@ export const HeaderTitle = ({
   children: string;
 }) => {
   return (
-    <h1 className={cn("text-5xl font-bold  md:text-6xl", className)}>
+    <h1 className={cn('text-5xl font-bold  md:text-6xl', className)}>
       {children}
     </h1>
   );
@@ -60,7 +52,7 @@ export const HeaderSubtitle = ({
 }) => {
   // the reason why the chidlren is wrapped in a article is because the children prop is a ReactNode, allowing for multiple elements(paragraphs, links, etc) to be passed in as children.
   return (
-    <p className={cn("text-base font-extralight", className)}>{children}</p>
+    <p className={cn('text-base font-extralight', className)}>{children}</p>
   );
 };
 
@@ -81,7 +73,7 @@ export const HeaderButton = ({
     <Link
       href={link}
       className={cn(
-        "w-fit rounded-lg border border-maroon bg-maroon px-4 py-1 text-2xl text-white transition-colors duration-300 ease-in-out hover:bg-gray",
+        'w-fit rounded-lg border border-maroon bg-maroon px-4 py-1 text-2xl text-white transition-colors duration-300 ease-in-out hover:bg-gray',
         className
       )}
       target={target}
