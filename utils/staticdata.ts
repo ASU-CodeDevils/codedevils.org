40fd92ec065beb402bdac9fd32dab14e77715d2e
David Nevarez <69279785+SloppyFlipFlop@users.noreply.github.com>
2024-05-02 23:21:05 -0700

    fix: Updates each officer's and project lead's avatar URL link and file formatting

--- utils/staticdata.ts
+++ utils/staticdata.ts
@@ -10,164 +10,164 @@ export type ProfileType = {
 
 export const officers = [
   {
-    name: "David Nevarez",
-    position: "President",
-    avatar: "/officers/david_nevarez.png",
+    name: 'David Nevarez',
+    position: 'President',
+    avatar: '/images/officers/david_nevarez.png',
     links: {
-      portfolio_url: "https://www.davidnevarez.info/",
-      linkedin: "https://www.linkedin.com/in/fernando-d-nevarez/",
+      portfolio_url: 'https://www.davidnevarez.info/',
+      linkedin: 'https://www.linkedin.com/in/fernando-d-nevarez/',
     },
   },
   {
-    name: "Frank Lin",
-    position: "Vice President",
-    avatar: "/officers/frank-lin.png",
+    name: 'Frank Lin',
+    position: 'Vice President',
+    avatar: '/images/officers/frank-lin.png',
     links: {
-      portfolio_url: "https://yellowrubberduckie.com/",
-      linkedin: "https://www.linkedin.com/in/frankjlin/",
+      portfolio_url: 'https://yellowrubberduckie.com/',
+      linkedin: 'https://www.linkedin.com/in/frankjlin/',
     },
   },
   {
-    name: "Haamid Juvale",
-    position: "Vice President",
-    avatar: "/officers/Haamid-Juvale.jpeg",
+    name: 'Haamid Juvale',
+    position: 'Vice President',
+    avatar: '/images/officers/Haamid-Juvale.jpeg',
     links: {
       portfolio_url:
-        "https://codedevils.notion.site/Haamid-Juvale-f21cb8be651445bfad01d2e175dcfda0?pvs=74",
-      linkedin: "https://www.linkedin.com/in/haamidjuvale/",
+        'https://codedevils.notion.site/Haamid-Juvale-f21cb8be651445bfad01d2e175dcfda0?pvs=74',
+      linkedin: 'https://www.linkedin.com/in/haamidjuvale/',
     },
   },
   {
-    name: "Tyler Kruer",
-    position: "Treasurer",
-    avatar: "/officers/Tyler-Kruer.png",
+    name: 'Tyler Kruer',
+    position: 'Treasurer',
+    avatar: '/images/officers/Tyler-Kruer.png',
     links: {
-      portfolio_url: "https://www.tkruer.com/",
-      linkedin: "https://www.linkedin.com/in/tkruer/",
+      portfolio_url: 'https://www.tkruer.com/',
+      linkedin: 'https://www.linkedin.com/in/tkruer/',
     },
   },
   {
-    name: "Ansh Dani",
-    position: "Treasurer",
-    avatar: "/officers/Ansh-Dani.png",
-    links: {
-      portfolio_url:
-        "https://codedevils.notion.site/Ansh-Dani-e0f7ac83afac446a988f38b63c3b8a27",
-      linkedin: "https://www.linkedin.com/in/ansh-dani/",
-    },
-  },
-  {
-    name: "Krishna Balaji",
-    position: "Secretary",
-    avatar: "/officers/Krishna-Balaji.png",
+    name: 'Ansh Dani',
+    position: 'Treasurer',
+    avatar: '/images/officers/Ansh-Dani.png',
     links: {
       portfolio_url:
-        "https://codedevils.notion.site/Krishna-Balaji-ef2b59b1aaff4e10a7bf8bd680d3964d",
-      linkedin: "https://www.linkedin.com/in/krishna-balaji-53785a257/",
+        'https://codedevils.notion.site/Ansh-Dani-e0f7ac83afac446a988f38b63c3b8a27',
+      linkedin: 'https://www.linkedin.com/in/ansh-dani/',
     },
   },
   {
-    name: "Pranith Molakalapalli",
-    position: "Project Coordinator",
-    avatar: "/officers/Pranith-Molakalapalli.jpeg",
-    links: {
-      portfolio_url: "https://prani.tech/",
-      linkedin: "https://www.linkedin.com/in/pranith-molakalapalli/",
-    },
-  },
-  {
-    name: "Mason Biegel",
-    position: "Project Coordinator",
-    avatar: "/officers/Mason-Biegel.png",
+    name: 'Krishna Balaji',
+    position: 'Secretary',
+    avatar: '/images/officers/Krishna-Balaji.png',
     links: {
       portfolio_url:
-        "https://codedevils.notion.site/Mason-Biegel-0d5e30317aab4ae1bec995c152c8c9e0?pvs=4",
-      linkedin: "https://www.linkedin.com/in/masonb3/",
+        'https://codedevils.notion.site/Krishna-Balaji-ef2b59b1aaff4e10a7bf8bd680d3964d',
+      linkedin: 'https://www.linkedin.com/in/krishna-balaji-53785a257/',
     },
   },
   {
-    name: "Remi Godin",
-    position: "Project Coordinator",
-    avatar: "/officers/Remi-Godin.png",
+    name: 'Pranith Molakalapalli',
+    position: 'Project Coordinator',
+    avatar: '/images/officers/Pranith-Molakalapalli.jpeg',
     links: {
-      portfolio_url:
-        "https://codedevils.notion.site/Remi-Godin-282225e0b6fe4b01948e707cf1a793f4?pvs=4",
-      linkedin: "https://www.linkedin.com/in/remi-godin-179719282/",
+      portfolio_url: 'https://prani.tech/',
+      linkedin: 'https://www.linkedin.com/in/pranith-molakalapalli/',
     },
   },
   {
-    name: "Isabella Faris",
-    position: "Marketing Coordinator",
-    avatar: "/officers/Isabella-Faris.png",
+    name: 'Mason Biegel',
+    position: 'Project Coordinator',
+    avatar: '/images/officers/Mason-Biegel.png',
     links: {
       portfolio_url:
-        "https://codedevils.notion.site/Isabella-Faris-abd5605cf2d24b2cb886ebc4e9abd088?pvs=4",
-      linkedin: "https://www.linkedin.com/in/isabellafaris/",
+        'https://codedevils.notion.site/Mason-Biegel-0d5e30317aab4ae1bec995c152c8c9e0?pvs=4',
+      linkedin: 'https://www.linkedin.com/in/masonb3/',
+    },
+  },
+  {
+    name: 'Remi Godin',
+    position: 'Project Coordinator',
+    avatar: '/images/officers/Remi-Godin.png',
+    links: {
+      portfolio_url:
+        'https://codedevils.notion.site/Remi-Godin-282225e0b6fe4b01948e707cf1a793f4?pvs=4',
+      linkedin: 'https://www.linkedin.com/in/remi-godin-179719282/',
+    },
+  },
+  {
+    name: 'Isabella Faris',
+    position: 'Marketing Coordinator',
+    avatar: '/images/officers/Isabella-Faris.png',
+    links: {
+      portfolio_url:
+        'https://codedevils.notion.site/Isabella-Faris-abd5605cf2d24b2cb886ebc4e9abd088?pvs=4',
+      linkedin: 'https://www.linkedin.com/in/isabellafaris/',
     },
   },
 ];
 
 export const projectLeads = [
   {
-    name: "David Nevarez",
-    position: "Website Project Lead",
-    avatar: "/officers/david_nevarez.png",
+    name: 'David Nevarez',
+    position: 'Website Project Lead',
+    avatar: '/images/officers/david_nevarez.png',
     links: {
-      portfolio_url: "https://www.davidnevarez.info/",
-      linkedin: "https://www.linkedin.com/in/fernando-d-nevarez/",
+      portfolio_url: 'https://www.davidnevarez.info/',
+      linkedin: 'https://www.linkedin.com/in/fernando-d-nevarez/',
     },
   },
 
   {
-    name: "Rhett Harrison",
-    position: "Website Project Lead",
-    avatar: "/project_leads/Rhett_Harrison.jpeg",
+    name: 'Rhett Harrison',
+    position: 'Website Project Lead',
+    avatar: '/images/project_leads/Rhett_Harrison.jpeg',
     links: {
-      portfolio_url: "https://www.rhettharrison.com/home",
-      linkedin: "https://www.linkedin.com/in/rhettharrison/",
+      portfolio_url: 'https://www.rhettharrison.com/home',
+      linkedin: 'https://www.linkedin.com/in/rhettharrison/',
     },
   },
   {
-    name: "Ian Castellanos",
-    position: "Discord Bot Project Lead",
-    avatar: "/project_leads/Ian_Castellanos.jpeg",
+    name: 'Ian Castellanos',
+    position: 'Discord Bot Project Lead',
+    avatar: '/images/project_leads/Ian_Castellanos.jpeg',
     links: {
-      linkedin: "https://www.linkedin.com/in/ian-castellanos-719a2115b/",
+      linkedin: 'https://www.linkedin.com/in/ian-castellanos-719a2115b/',
     },
   },
   {
-    name: "Jason Michelson",
-    position: "Discord Bot Project Lead",
-    avatar: "/project_leads/Jason_Michelson.jpeg",
+    name: 'Jason Michelson',
+    position: 'Discord Bot Project Lead',
+    avatar: '/images/project_leads/Jason_Michelson.jpeg',
     links: {
-      linkedin: "https://www.linkedin.com/in/jasonmichelson/",
+      linkedin: 'https://www.linkedin.com/in/jasonmichelson/',
     },
   },
   {
-    name: "Remi Godin",
-    position: "Discord Bot Project Lead",
-    avatar: "/officers/Remi-Godin.png",
+    name: 'Remi Godin',
+    position: 'Discord Bot Project Lead',
+    avatar: '/images/officers/Remi-Godin.png',
     links: {
       portfolio_url:
-        "https://codedevils.notion.site/Remi-Godin-282225e0b6fe4b01948e707cf1a793f4?pvs=4",
-      linkedin: "https://www.linkedin.com/in/remi-godin-179719282/",
+        'https://codedevils.notion.site/Remi-Godin-282225e0b6fe4b01948e707cf1a793f4?pvs=4',
+      linkedin: 'https://www.linkedin.com/in/remi-godin-179719282/',
     },
   },
   {
-    name: "Frank Lin",
-    position: "React Components Library Project Lead",
-    avatar: "/officers/frank-lin.png",
+    name: 'Frank Lin',
+    position: 'React Components Library Project Lead',
+    avatar: '/images/officers/frank-lin.png',
     links: {
-      portfolio_url: "https://yellowrubberduckie.com/",
-      linkedin: "https://www.linkedin.com/in/frankjlin/",
+      portfolio_url: 'https://yellowrubberduckie.com/',
+      linkedin: 'https://www.linkedin.com/in/frankjlin/',
     },
   },
 ];
 
 export const socialLinks = {
-  discord: "https://discord.gg/eevXKjVmm2",
-  sundevilsync: "https://asu.campuslabs.com/engage/organization/codedevils/",
-  linkedin: "https://www.linkedin.com/company/codedevils-official/",
-  instagram: "https://www.instagram.com/codedevils.asu/",
-  github: "https://github.com/ASU-CodeDevils/",
+  discord: 'https://discord.gg/eevXKjVmm2',
+  sundevilsync: 'https://asu.campuslabs.com/engage/organization/codedevils/',
+  linkedin: 'https://www.linkedin.com/company/codedevils-official/',
+  instagram: 'https://www.instagram.com/codedevils.asu/',
+  github: 'https://github.com/ASU-CodeDevils/',
 };
