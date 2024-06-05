759b2696807ff73ccee0b8d2c55e9b5fda1676ce
David Nevarez <69279785+SloppyFlipFlop@users.noreply.github.com>
2024-06-05 11:53:26 -0700

    chore: Updates robots and sitemap files with new URL
    With the URL change, this update revises our robots and sitemap files to contain the new URL.

--- app/robots.ts
+++ app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '',
     },
-    sitemap: 'https://www.codedevils.org/sitemap.xml',
+    sitemap: 'https://www.codedevils.io/sitemap.xml',
   };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,37 +5,37 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.codedevils.org',
+      url: 'https://www.codedevils.io',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://www.codedevils.org/get-started',
+      url: 'https://www.codedevils.io/get-started',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://www.codedevils.org/projects',
+      url: 'https://www.codedevils.io/projects',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.7,
     },
     {
-      url: 'https://www.codedevils.org/support',
+      url: 'https://www.codedevils.io/support',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.6,
     },
     {
-      url: 'https://www.codedevils.org/about',
+      url: 'https://www.codedevils.io/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.9,
     },
     {
-      url: 'https://www.codedevils.org/careers',
+      url: 'https://www.codedevils.io/careers',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.5,
