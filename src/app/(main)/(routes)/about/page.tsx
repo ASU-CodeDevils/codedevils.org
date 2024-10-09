172c6736f297026646b5bb9e4eb698ff39221b83
Pita <pita.sherwood@protonmail.com>
2024-10-09 04:03:26 -0500

    fixing build error (test)

--- src/app/(main)/(routes)/about/page.tsx
+++ src/app/(main)/(routes)/about/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import type { Metadata, NextPage } from 'next';
+import type { Metadata } from 'next';
 
 import { HoverEffect } from 'src/app/components/ui/card-hover-effect';
 import { Hero, Section } from 'src/app/components/ui/ContentWrappers';
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
   },
 };
 
-const AboutPage: NextPage = () => {
+const AboutPage = () => {
   return (
     <>
       <Hero className='bg-gray'>
