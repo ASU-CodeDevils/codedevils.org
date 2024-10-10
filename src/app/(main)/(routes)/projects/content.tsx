c93c458f4bb2d87385813eee3a9ec3f86efbc322
PattyBird31 <byrd.pat16@gmail.com>
2024-10-09 19:19:10 -0500

    Updating members

--- src/app/(main)/(routes)/projects/content.tsx
+++ src/app/(main)/(routes)/projects/content.tsx
@@ -35,7 +35,8 @@ const projectTeams = {
     academy: [['pita'],
             ],
     website: [  ['pita'],
-                ['David Fales']
+                ['David Fales'],
+                ['Juliana Bush']
             ],
     gamedev: [  ['remi'],
                 ['Autumn O'],
