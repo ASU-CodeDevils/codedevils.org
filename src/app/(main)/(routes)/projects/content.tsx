22b2cda93d744b30dbb1b263c4aff8f9330958dc
Ella Rushing <ellarushing2022@gmail.com>
2024-10-16 19:18:13 -0700

    Added to mobile team

--- src/app/(main)/(routes)/projects/content.tsx
+++ src/app/(main)/(routes)/projects/content.tsx
@@ -41,7 +41,10 @@ const projectTeams = {
                 ['Autumn O'],
                 ['Pita Sherwood', 'https://pita.blue'],
             ],
-    mobile: [['pita']
+    mobile: [['pita'],
+            ['John Miller'],
+            ['Alejandro Jimenez'],
+            ['Korbin Hillan']
             ],
     scraper: [['frankie']
             ]
